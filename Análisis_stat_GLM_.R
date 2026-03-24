12f38ae2110d33f2dc41413ef836e80252b41e86
SJack2023 <barbozasaldanaj@gmail.com>
2026-03-23 21:15:18 -0500

    avance
    Graficas

--- Análisis_stat_GLM_.R
+++ Análisis_stat_GLM_.R
@@ -9,90 +9,63 @@ library(gridExtra)
 library(stats)
 library(visreg)
 
+#2. Cargar datos   
+bandas <- read.csv("datos_p.csv", header = TRUE, row.names = 1)
+manchas <- read.csv("sin_patrones.csv", header = TRUE, row.names = 1)
 
-###### Anális en zona con patrones ######
-datos_p <- read.csv("datos_p.csv", header = TRUE, row.names = 1)
-str(datos_p)
-View(datos_p)
+#3.Media y desviación estándar
+#3.1 Para bandas
+medias_b <- sapply(bandas[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], mean)
+ds_b <- sapply(bandas[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], sd)
+#3.2 Para manchas
+medias_m <- sapply(manchas[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], mean)
+ds_m <- sapply(manchas[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], sd)
 
-# Test de normalidad 
-pruebas <- c("H_den", "H_fue", "T_den", "T_fue", "Pen", "N_ind", "T_par")
-shapiro_tests1 <- lapply(datos_p[pruebas], shapiro.test)
+#4 Comparacion de la temperatura y humedad
+#4.1 Para bandas
+# Normalidad
+var_p <- c("V_te", "V_hu")
+shapiro_tests1 <- lapply(bandas[var_p], shapiro.test)
+shapiro_tests1
+# Prueba de Wilcoxon pareado 
+twip <- wilcox.test(bandas$T_den, bandas$T_fue, paired = TRUE)
+hwip <- wilcox.test(bandas$H_den, bandas$H_fue, paired = TRUE)
+
+
+# 4.2 Para manchas
+#Normalidad
+var_s <- c("V_te","V_hu")
+shapiro_tests1 <- lapply(manchas[var_s], shapiro.test)
 shapiro_tests1
 
-# Prueba de Levene para homocedasticidad
-datos_p$Gru <- as.factor(datos_p$Gru)
-
-variables1 <- c("H_den", "H_fue", "T_den", "T_fue", "Pen", "N_ind", "T_par", "N_par")
-
-levene_results1 <- lapply(variables1, function(var) {
-  formula <- as.formula(paste(var, "~ Gru"))
-  leveneTest(formula, data = datos_p)
-})
-
-names(levene_results1) <- variables1 
-levene_results1
-
-datos_p$Gru <- NULL  ## eliminar columna 
-View(datos_p)
+# Prueba de Wilcoxon pareado 
+tWis <- wilcox.test(manchas$T_den, manchas$T_fue, paired = TRUE)
+hwis <- wilcox.test(manchas$H_den, manchas$H_fue, paired = TRUE)
 
 
-hum <- datos_p$V_hu 
-tem <- datos_p$V_te
-pen <- datos_p$Pen
+#5. Intervalos de confianza
+#5.1 Error estandar
+n_plots <- length(manchas$T_par)
 
-t_pa<- datos_p$T_par
-n_in <- datos_p$N_ind
-n_pa <- datos_p$N_par
+SEs_bandas <- ds_b/sqrt(n_plots)
+CIs_bandas <- qt(.975, df=n_plots - 1) * SEs_bandas
 
-# Analisis exploratorio 
-
-list(Humedad = t.test(datos_p$V_hu, s_pa$V_hu), Temperatura = t.test(datos_p$V_te, s_pa$V_te))
-par(mfrow = c(1, 2))
-
-boxplot(datos_p$V_hu,  s_pa$V_hu, col=c("skyblue", "orange"), main="Humedad", names=c("Bandas", "Manchas"))
-boxplot(datos_p$V_te, s_pa$V_te , col=c("blue", "red"), main="Temperatura", names=c("B ", "M"))
+SEs_manchas <- ds_m/sqrt(n_plots)
+CIs_manchas <- qt(.975, df=n_plots - 1) * SEs_manchas
 
 
-
-pru_Whi <- wilcox.test(datos_p$T_den, datos_p$T_fue)
-
-#Media y desviación estándar
-sapply(datos_p[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], mean)
-sapply(datos_p[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], sd)
-
-
-
-# Media y desviación estándar. Irbin
-data_spotted <- read.csv("datos_p.csv", header = TRUE, row.names = 1)
-data_striped <- read.csv("sin_patrones.csv", header = TRUE, row.names = 1)
-
-n_plots <- length(data_spotted$Gru)
-means_spotted <- sapply(data_spotted[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], mean)
-stdes_spotted <- sapply(data_spotted[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], sd)
-
-means_striped <- sapply(data_striped[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], mean)
-stdes_striped <- sapply(data_striped[c("V_te", "V_hu", "Pen", "N_ind", "T_par", "N_par")], sd)
-
-# Intervalos de confianza
-SEs_spotted <- stdes_spotted/sqrt(n_plots)
-CIs_spotted <- qt(.975, df=n_plots - 1) * SEs_spotted
-
-SEs_striped <- stdes_striped/sqrt(n_plots)
-CIs_striped <- qt(.975, df=n_plots - 1) * SEs_striped
-
-# Graficos
+#5.2 Graficos de los intervalos de confianza
 vars <- c("Variación temperatura", "Variación humedad", "Pendiente",
           "Número de individuos", "Longitud parche", "Número de parches")
 
 df_plot <- data.frame(
   variable = rep(vars, 2),
-  media = c(means_spotted, means_striped),
-  CI = c(CIs_spotted, CIs_striped),
-  grupo = rep(c("Manchas", "Bandas"), each = length(vars))
+  media = c(medias_b, medias_m),
+  CI = c(CIs_bandas, CIs_manchas),
+  grupo = rep(c("Bandas", "Manchas"), each = length(vars))
 )
 
-df_plot$grupo <- factor(df_plot$grupo, levels = c("Manchas", "Bandas"))
+df_plot$grupo <- factor(df_plot$grupo, levels = c("Bandas", "Manchas"))
 
 ggplot(df_plot, aes(x = grupo, y = media, color = grupo)) +
   geom_point(size = 3) +
@@ -102,7 +75,7 @@ ggplot(df_plot, aes(x = grupo, y = media, color = grupo)) +
              scales = "free_y", strip.position = "left",
              labeller = labeller(variable = vars)) +  
   labs(color = "Tipo de patrón", x = NULL, y = NULL)+
-  scale_color_manual(values = c("Manchas" = "blue", "Bandas" = "red"))+
+  scale_color_manual(values = c("Bandas" = "blue", "Manchas" = "red"))+
   scale_y_continuous(n.breaks = 5) +
   theme_minimal() +
   theme(axis.text.x = element_blank())+
@@ -110,6 +83,131 @@ ggplot(df_plot, aes(x = grupo, y = media, color = grupo)) +
         strip.placement = "outside",        
         strip.text = element_text(hjust = 0.5, face = "bold", vjust = 0.5),
         panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5))
+
+#6. Permanova
+library(vegan)
+data_total <- rbind(bandas[5:10], manchas[5:10])
+data_total$Gru <- rep(c("Bandas", "Manchas"), each = n_plots)
+data_varnames <- colnames(data_total)
+scale_data <- scale(data_total[, data_varnames[-7]])
+
+adonis2(scale_data ~ Gru, data = data_total, permutations = 999, method = "euclidean")
+
+#7. PCA
+pca_data <- prcomp(scale_data, center = TRUE)
+summary(pca_data)
+
+#7.1 PCA plot
+pca_scores <- as.data.frame(pca_data$x)
+pca_scores$Gru <- data_total$Gru
+
+ggplot(pca_scores, aes(x = PC1, y = PC2, color = Gru))+
+  geom_point(size = 3)+
+  stat_ellipse(level = 0.95) +
+  labs(x = "PC1", y = "PC2", color = "Tipo de patrón")+
+  coord_cartesian(xlim = c(-1.7*max(abs(pca_scores$PC1)), 1.7*max(abs(pca_scores$PC1))),
+                  ylim = c(-1.5*max(abs(pca_scores$PC2)), 1.5*max(abs(pca_scores$PC2))))+
+  theme_minimal()+
+  scale_color_manual(values = c("Bandas"="blue", "Manchas"="red"))+
+  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5))
+
+#7.2 Biplot
+pca_loads <- as.data.frame(pca_data$rotation)
+pca_loads$var <- c("VT", "VH", "P",
+                   "NI", "LP", "NP")
+
+ggplot(pca_scores, aes(x = PC1, y = PC2, color = Gru))+
+  geom_point(size = 3)+
+  stat_ellipse(level = 0.95) +
+  geom_segment(data = pca_loads,
+               aes(x = 0, y = 0, xend = PC1*2.5, yend = PC2*2.5),
+               arrow = arrow(length = unit(.2, "cm")),
+               color = "black",
+               inherit.aes = FALSE) +
+  geom_text(data = pca_loads,
+            aes(x = PC1*2, y = PC2*3, label=var),
+            color = "black",
+            size = 3,
+            inherit.aes = FALSE)+
+  labs(x = "PC1", y = "PC2", color = "Tipo de patrón")+
+  theme_minimal()+
+  scale_color_manual(values = c("Bandas"="blue", "Manchas"="red"))+
+  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5))
+
+#simper(scale_data, data_total$Gru)
+
+## Regresion logistica
+std_data <- as.data.frame(scale_data)
+std_data$Gru <- factor(data_total$Gru)
+
+log_model <- glm(Gru ~ V_te + V_hu + Pen,
+                 data = std_data,
+                 family = binomial)
+summary(log_model)
+
+## Logistic Regressión Plot
+newdata <- data.frame(
+  V_te = seq(min(std_data$V_te), max(std_data$V_te), length.out = 200),
+  V_hu = 0,
+  Pen = 0)
+
+predicts <- predict(log_model, newdata = newdata, type = "link", se.fit = TRUE)
+
+fit_link <- predicts$fit
+se_fit <- predicts$se.fit
+
+CI_lower <- fit_link - 1.96 * se_fit
+CI_upper <- fit_link + 1.96 * se_fit
+
+newdata$probs <- plogis(fit_link)
+newdata$CI_lower <- plogis(CI_lower)
+newdata$CI_upper <- plogis(CI_upper)
+
+ggplot(std_data, aes(x = V_te, y = as.numeric(Gru)-1)) +
+  geom_point(aes(color = Gru),
+             position = position_jitter(height = .01)) +
+  geom_ribbon(data = newdata,
+              aes(x = V_te, ymin = CI_lower, ymax = CI_upper),
+              alpha = .2,
+              fill = "gray50",
+              inherit.aes = FALSE) +
+  geom_line(data = newdata,
+            aes(x = V_te, y = probs),
+            color = "black",
+            linewidth = 1) +
+  labs(x = "Variación temperatura (estandarizada)",
+       y = "Probabilidad",
+       color = "Tipo de patrón") +
+  theme_minimal() +
+  scale_color_manual(values = c("Bandas" = "blue", "Manchas" = "red"))+
+  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5))
+
+
+
+
+
+
+
+
+
+
+
+
+hum <- datos_p$V_hu 
+tem <- datos_p$V_te
+pen <- datos_p$Pen
+hu_d <- datos_p$H_den
+hu_f <- datos_p$H_fue
+te_f <- datos_p$T_fue
+te_d <- datos_p$T_den
+
+t_pa<- datos_p$T_par
+n_in <- datos_p$N_ind
+n_pa <- datos_p$N_par
+
+
+
+
 
 ##### R lineal múltiple tamaño de parches vs f. abióticos  #####
 modelo_1 <- lm(t_pa ~ tem + hum + pen, data = datos_p)
@@ -199,7 +297,7 @@ print(plot_final)
 
 
 #### Modelo de Poisson en zona con patrones ####
-M1 <- glm(n_in ~ hum_d+hum_f+tem_d+tem_f+pen, 
+M1 <- glm(n_in ~ hum + tem +pen, 
           data = datos_p, 
           family = poisson(link = "log")) 
 summary(M1)
@@ -395,14 +493,14 @@ hum_f2 <- s_pa$H_fue
 hum_d2 <- s_pa$H_den 
 tem_d2 <- s_pa$T_den
 tem_f2 <- s_pa$T_fue 
+
+hum2 <- s_pa$V_hu
+tem2 <- s_pa$V_te
 pen2 <- s_pa$Pen
 N_pa<- s_pa$N_par
 N_in <- s_pa$N_ind
 T_pa <- s_pa$T_par
 
-##  Analisis exploratorio ###
-#T-student
-list(humedad = t.test(hum_d2, hum_f2, data = s_pa), temperatura = t.test(tem_d2, tem_f2, data = s_pa))
 
 # Prueba de Levene para homocedasticidad
 s_pa$Gru <- as.factor(s_pa$Gru)
@@ -423,29 +521,37 @@ View(s_pa)
 pru_Whi2 <- wilcox.test(s_pa$T_den, s_pa$T_fue, exact = FALSE)
 pru_Whi2
 
+#### T-test pareado 
+t_test3 <- t.test(s_pa$T_den, s_pa$T_fue,
+                  paired = TRUE,
+                  conf.level = 0.95)
+print(t_test3)
+t_test4 <- t.test(s_pa$H_den, s_pa$H_fue,
+                  paired = TRUE,
+                  conf.level = 0.95)
+print(t_test4)
+
 
 ### Regresión lineal múltiple tamaño de parches vs f. abióticos ###
-modelo_3 <- lm(T_pa ~ tem_d2 + tem_f2 + hum_d2 + hum_f2 + pen2, data = s_pa)
+modelo_3 <- lm(T_pa ~ tem2 + hum2 + pen2, data = s_pa)
 summary(modelo_3)  
 ## Metodo stepwise
 step(modelo_3, direction = "backward", test = "F") 
-modelo_4 <- lm(T_pa ~ hum_d2 + hum_f2 + pen2, data = s_pa)
+modelo_4 <- lm(T_pa ~ hum2, data = s_pa)
 summary(modelo_4) 
 
 ## PLot 
 
-
+library(patchwork)
 # tamaño de parche en zona sin patrones vs v abioticas
-pred_hum_d2 <- ggpredict(modelo_4, terms = "hum_d2")
-pred_hum_f2 <- ggpredict(modelo_4, terms = "hum_f2")
-pred_pen2 <- ggpredict(modelo_4, terms = "hum_f2")
+pred_hum_2 <- ggpredict(modelo_4, terms = "hum2")
 
-p1 <- ggplot(pred_hum_d2, aes(x = x, y = predicted)) +
+p1 <- ggplot(pred_hum_2, aes(x = x, y = predicted)) +
   geom_line(color = "#2c3e50", size = 1) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2, fill = "#2c3e50") +
   labs(
     title = " ",
-    x = " HDP ",
+    x = " Variacion de la humedad ",
     y = "Tamaño del parche"
   ) +
   theme_minimal() +
@@ -456,23 +562,7 @@ p1 <- ggplot(pred_hum_d2, aes(x = x, y = predicted)) +
     axis.text.y = element_text(size = 13)     # Números eje Y
   )
 
-p2 <- ggplot(pred_hum_f2, aes(x = x, y = predicted)) +
-  geom_line(color = "#e74c3c", size = 1) +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2, fill = "#e74c3c") +
-  labs(
-    title = " ",
-    x = " HFP ",
-    y = "Tamaño del parche"
-  ) +
-  theme_minimal() +
-  theme(
-    axis.title.x = element_text(size = 13),
-    axis.title.y = element_text(size = 13),
-    axis.text.x = element_text(size = 13),
-    axis.text.y = element_text(size = 13)
-  )
-
-grafico_final <- p1 + p2 
+grafico_final <- p1 
   plot_annotation(
     title = ' ',
     subtitle = ' ',
@@ -485,11 +575,9 @@ print(grafico_final)
 
 ## Plot  modelo final
 # 1. Generar predicciones 
-pred_h_f  <- ggpredict(modelo_4, terms = "hum_f2")
-pred_h_d  <- ggpredict(modelo_4, terms = "hum_d2")
-pred_pen2 <- ggpredict(modelo_4, terms = "pen2")
+pred_h_2  <- ggpredict(modelo_4, terms = "hum2")
 
-plot_lm_pro <- function(preds, original_data, x_var, title, xtitle) {
+plot_lm <- function(preds, original_data, x_var, title, xtitle) {
   ggplot() +
     # Datos observados (puntos)
     geom_jitter(data = original_data, 
@@ -522,15 +610,10 @@ plot_lm_pro <- function(preds, original_data, x_var, title, xtitle) {
 }
 
 #  Crear gráficos individuales
-p1 <- plot_lm_pro(pred_h_f, s_pa, "hum_f2", " ", "HFP (%)")
-p2 <- plot_lm_pro(pred_h_d, s_pa, "hum_d2", " ", "HDP (%)")
-p3 <- plot_lm_pro(pred_pen2, s_pa, "pen2", " ", " Pendiente (grad)")
+p1 <- plot_lm(pred_h_2, s_pa, "hum2", " ", "Variación de humedad")
 
-#  Quitar etiquetas repetidas 
-p2 <- p2 + theme(axis.title.y = element_blank(), axis.text.y = element_blank())
-
-# Combinación Final (2 arriba, 1 abajo con mismo ancho)
-plot_final <- (p1 + p2) / p3 + 
+# Combinación Final 
+plot_final <- p1 +  
   plot_layout(heights = c(1, 1)) + # Filas de igual altura
   plot_annotation(
     title = " ",
@@ -543,7 +626,7 @@ print(plot_final)
 
 #### Modelo Poisson sin_patrones #### 
 
-M6 <- glm(N_in ~ hum_d2 + hum_f2 + tem_d2 + tem_f2 + pen2, 
+M6 <- glm(N_in ~ hum2 + tem2 + pen2, 
           data = s_pa, 
           family = poisson(link = "log"))
 summary(M6)
@@ -764,8 +847,13 @@ summary(M11)
 pseudo_R6 <- (M11$null.deviance-M11$deviance)/M11$null.deviance*100
 pseudo_R6
 
-#Graph
 
+M12 <- glm(total_pa$Pre_aus ~ tem+ hum+ pen + N_in +N_pa +T_pa, 
+           data = total_pa, family = binomial)
+summary(M12)
+
+
+#Graph
 library(ggplot2)
 library(visreg)
 library(patchwork)
